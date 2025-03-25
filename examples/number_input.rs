741dadc69324207eebbdff127c7fa62d0db13ad2
Trung Do <dothanhtrung@pm.me>
2025-03-26 00:40:29 +0700

    Fix cargo test examples when feature experimental is not enabled

--- examples/number_input.rs
+++ examples/number_input.rs
@@ -1,28 +1,33 @@
-#![cfg(feature = "experimental")]
-
+#[cfg(feature = "experimental")]
 use bevy::color::palettes::tailwind::{NEUTRAL_600, ZINC_800};
 use bevy::prelude::*;
 use bevy::window::WindowResolution;
+#[cfg(feature = "experimental")]
 use bevy_text_edit::experimental::number_input::{spawn_number_input_text, NumberInputChanged, NumberInputSetting};
 use bevy_text_edit::TextEditPluginNoState;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                resolution: WindowResolution::new(400., 300.),
-                ..default()
-            }),
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            resolution: WindowResolution::new(400., 300.),
             ..default()
-        }))
-        .add_plugins(TextEditPluginNoState)
-        .add_systems(Startup, setup)
-        .run();
+        }),
+        ..default()
+    }))
+    .add_plugins(TextEditPluginNoState);
+
+    #[cfg(feature = "experimental")]
+    app.add_systems(Startup, setup);
+
+    app.run();
 }
 
+#[cfg(feature = "experimental")]
 #[derive(Component)]
 struct Result;
 
+#[cfg(feature = "experimental")]
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
 
@@ -51,6 +56,7 @@ fn setup(mut commands: Commands) {
     commands.entity(id.unwrap()).observe(get_result);
 }
 
+#[cfg(feature = "experimental")]
 fn get_result(trigger: Trigger<NumberInputChanged>, mut query: Query<&mut Text, With<Result>>) {
     for mut text in query.iter_mut() {
         **text = format!("Result: {}", trigger.0);
